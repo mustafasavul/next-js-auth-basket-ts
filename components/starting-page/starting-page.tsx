85a786f28cc2130674c7a9191dd159c946ea793a
Mustafa Savul <mustafa.savul@crowdsol.com>
2022-10-22 01:38:06 +0300

    feat: add react-lottie

--- components/starting-page/starting-page.tsx
+++ components/starting-page/starting-page.tsx
@@ -1,11 +1,14 @@
+import Lottie from 'react-lottie-player';
 import classes from './starting-page.module.css';
+import lottieJson from 'public/assets/isometric-house.json';
 
 function StartingPageContent() {
   // Show Link to Login page if NOT auth
 
   return (
     <section className={classes.starting}>
-      <h1>Welcome on Board!</h1>
+      <h3>Yeni Nesil Emlak Pazar Yeri</h3>
+      <Lottie loop animationData={lottieJson} play />
     </section>
   );
 }
